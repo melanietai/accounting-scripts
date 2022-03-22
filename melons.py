9b04c12ca820d72a937ad23349ac1b9b1f94b9e9
Melanie Tai <melon125@gmail.com>
2022-03-21 22:31:41 -0700

    Modify melon.py info into nested dictionary

--- melons.py
+++ melons.py
@@ -1,23 +1,62 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
+
+melon_data = {
+    'Honeydew': {
+        'price': 0.99,
+        'seeless': True,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None
+        },
+    'Crenshaw': {
+        'price': 2.00,
+        'seeless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None
+        },
+    'Crane': {
+        'price': 2.50,
+        'seeless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None
+        },
+    'Casaba': {
+        'price': 2.50,
+        'seeless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None
+        },
+    'Cantaloupe': {
+        'price': 0.99,
+        'seeless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None
+        },
+    }
+
